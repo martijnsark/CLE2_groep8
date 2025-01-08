c0e149ccd781615ef7b5603c31bebb6cd3fe00fe
martiijn <m.van.der.willigen@sintlucasedu.nl>
2025-01-08 11:41:48 +0100

    php store

---
+++ store.php
@@ -0,0 +1,29 @@
+<?php
+// Include the database connection
+include 'includes/connection.php';
+//ignore error
+/** @var mysqli $db */
+
+// getting posted content from form.html
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    $firstname = $_POST ['naam'];
+    $lastname = $_POST ['achternaam'];
+    $email = $_POST ['name'];
+
+
+    //sent the posted data to the list of reservations
+    $query  = "INSERT INTO reservations (firstname, lastname, email)
+               VALUES ('$firstname', '$lastname', '$email')";
+    mysqli_query($db, $query);
+
+
+
+    // Redirect back to index.html
+    header('Location: index.html');
+    exit();
+}
+
+ 
+
+//close connection with database
+mysqli_close($db);
