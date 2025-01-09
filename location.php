ad5aec0fa6ddf98ce0afbd4890b64f64263dacfb
martiijn <m.van.der.willigen@sintlucasedu.nl>
2025-01-09 11:15:24 +0100

    update

---
+++ location.php
@@ -0,0 +1,79 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Location</title>
+    <link rel="stylesheet" href="css/style.css">
+    <link href="https://fonts.cdnfonts.com/css/imagination-station" rel="stylesheet">
+    <link href="https://fonts.cdnfonts.com/css/open-sans" rel="stylesheet">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+</head>
+<body>
+<nav>
+    <div class="left-column">
+        <img src="images/Shabu_Shabu_Logo.png" alt="ShabuShabu Logo">
+    </div>
+    <div class="right-column">
+        <a href="index.html">Home</a>
+        <a href="#">Over ons</a>
+        <a href="reserveren.html">Reserveren</a>
+        <a href="#">Contact</a>
+    </div>
+</nav>
+<header>
+    <div class="center-container">
+        <div class="half-container">
+            <h1 class="shabu-title">Shabu Shabu</h1>
+            <p>
+                Welkom bij sushi restaurant <strong>SHABU SHABU</strong>! U kunt bij ons onbeperkt sushi eten voor een
+                vaste prijs. Of u nu komt voor een lunch of een diner. Onze chef-koks bereiden altijd de lekkerste sushi
+                met dagverse ingrediënten. Schuif dus heerlijk aan in één van onze sushi restaurants in de buurt of
+                bestel uw sushi eenvoudig online.
+            </p>
+            <p>
+                <strong>UITBREIDING VAN ONZE ALL-YOU-CAN-EAT AVONDEN!</strong><br>
+                Beste gasten, we hebben geweldig nieuws! Om jullie nog meer te laten genieten van onze heerlijke
+                sushi
+                en
+                andere gerechten is het All-You-Can-Taste diner van maandag t/m donderdag nu de hele avond onbeperkt
+                zitten.
+                (m.u.v. andere arrangementen & feestdagen, informeer bij de vestiging)
+            </p>
+            <p>
+                We kijken ernaar uit jullie binnenkort te verwelkomen en samen van een ontspannen en smaakvolle
+                avond te
+                genieten.
+            </p>
+        </div>
+        <div class="half-container">
+            <form action="">
+                <label for="location">Kies een Locatie:</label>
+                <select id="location" name="location" required>
+                    <option value="" disabled selected>Locatie</option>
+                    <option value="Alphen aan den Rijn">Alphen aan den Rijn</option>
+                    <option value="Amersfoort">Amersfoort</option>
+                    <option value="Apeldoorn">Apeldoorn</option>
+                    <option value="Den Haag">Den Haag</option>
+                    <option value="Ede">Ede</option>
+                    <option value="Leiden">Leiden</option>
+                    <option value="Rotterdam">Rotterdam</option>
+                    <option value="Rotterdam Alexandrium">Rotterdam Alexandrium</option>
+                    <option value="Rotterdam Zuid">Rotterdam Zuid</option>
+                    <option value="Zoetermeer">Zoetermeer</option>
+                </select>
+
+                <button type="submit">Kies locatie</button>
+            </form>
+
+
+        </div>
+    </div>
+</header>
+<footer>
+    <div class="foot-icons">
+        <a href="https://www.facebook.com/shabushabu.nl/?locale=nl_NL" class="fa fa-facebook"></a>
+        <a href="https://www.instagram.com/restaurant_shabushabu/?hl=nl" class="fa fa-instagram"></a>
+    </div>
+</footer>
+</body>
+</html>
