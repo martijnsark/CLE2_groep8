9905abbbe92eaa42536a9401630e23bb09796b8b
martiijn <m.van.der.willigen@sintlucasedu.nl>
2025-01-10 14:32:10 +0100

    functional account creation demo

--- login_test/login.php
+++ login_test/login.php
@@ -2,13 +2,29 @@
 
 INCLUDE_ONCE 'header.php';
 
-
 ?>
 
 
-
+<header>
+    <div class="header-content">
+        <section class ="login-form">
+            <h2>Log In</h2>
+            <form action="includes/login.inc.php" method="POST"> 
+                <input type="text" name="name" placeholder="Username/email...">
+                <input type="password" name="pwd" placeholder="Password...">
+                <button type="submit" name="sumbit">Log In<button>
+            <form>
+        </section>
+    </div>
+</header>
+<main>
+    
+</main>
 
 <?php
 INCLUDE_ONCE 'footer.php';
 
 ?>
+
+</body>
+</html>
--- login_test/signup.php
+++ login_test/signup.php
@@ -8,11 +8,14 @@ INCLUDE_ONCE 'header.php';
 <header>
     <div class="header-content">
         <section class ="signup-form">
-            <h2>sign up</h2>
-            <form action="signup.inc.php" method="POST"> 
-                <input type="text" name="fullname" placeholder="Full name...">
+            <h2>Sign Up here</h2>
+            <form action="includes/signup.inc.php" method="POST"> 
+                <input type="text" name="name" placeholder="Full name...">
                 <input type="email" name="email" placeholder="Email...">
-                <input type="password" name="password" placeholder="Password...">
+                <input type="text" name="uid" placeholder="Username...">
+                <input type="password" name="pwd" placeholder="Password...">
+                <input type="password" name="pwdrepeat" placeholder="Repeat password...">
+                <button type="submit" name="submit">Sign Up<button>
             <form>
         </section>
     </div>
