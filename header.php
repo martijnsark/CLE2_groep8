d0af8fa647d789d2d3630905960c7b7fe0b61b32
martiijn <m.van.der.willigen@sintlucasedu.nl>
2025-01-13 15:44:07 +0100

    updated the nav to the form to index as index is form

--- header.php
+++ header.php
@@ -17,7 +17,7 @@
     <div class="right-column">
         <a href="index.php">Home</a>
         <a href="about.php">Over ons</a>
-        <a href="form.php">Reserveren</a>
+        <a href="index.php">Reserveren</a>
         <a href="contact.php">Contact</a>
         <a href="signup.php">Sign up</a>
         <a href="login.php">Log in</a>
