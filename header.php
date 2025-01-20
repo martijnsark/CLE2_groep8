e825746cd88268d575b90cf279d94411bfd9c504
Martijn <m.van.der.willigen@sintlucasedu.nl>
2025-01-20 15:56:42 +0100

    Update header.php

--- header.php
+++ header.php
@@ -16,7 +16,6 @@
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
 
 </head>
-<body>
 <nav>
     <div class="left-column">
         <img src="images/Shabu_Shabu_Logo.png" alt="ShabuShabu Logo">
